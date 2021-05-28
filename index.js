a9d98cf5374ff9d732732ed709c5f63dc1860cc4
Anna Zocher <anna.zocher@gmail.com>
2021-05-28 10:20:50 -0700

    stubed out basic routes

---
+++ index.js
@@ -0,0 +1,26 @@
+// initial express app setup
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// GET route for /
+app.get('/', (req, res) => {
+    res.send("🐛")
+    // res.sendFile to render index.html
+})
+
+// GET route for /about
+app.get('/about', (req, res) => {
+    res.send("👾")
+})
+
+// GET route for /blog for blog posts
+app.get('/blog', (req, res) => {
+    res.send("🐳")
+})
+
+// open up port for app to be listening on
+app.listen(PORT, (err) => {
+    if (err) { console.log(err) }
+    console.log(`You are tuned in to the sweet sweet sounds of ${PORT} this morning ☕️`)
+})
